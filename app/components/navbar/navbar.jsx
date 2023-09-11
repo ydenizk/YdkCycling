e85be7356d23edea64c5ebabd0254dde3f6ab889
Denız  Kurt <denizkurt@192.168.1.66>
2023-09-11 16:02:26 +0300

    finito

--- app/components/navbar/navbar.jsx
+++ app/components/navbar/navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-//import { UserButton, auth } from "@clerk/nextjs";
+
 
 const menu = [
   {
@@ -26,8 +26,6 @@ const menu = [
 ];
 
 function Navbar() {
-  //const { userId } = auth();
-
   return (
     <nav
       className="w-full h-20 bg-zinc-900 px-4 py-2 flex items-center  
@@ -55,48 +53,6 @@ function Navbar() {
               </li>
             );
           })}
-
-          {/* <div className="flex justify-center items-center">
-            <li className="ml-8 ">
-              <Link
-                href="/sign-in"
-                className="border border-slate-400 text-sm p-2  rounded-2xl bg-slate-700  hover:bg-slate-800 transition"
-              >
-                Sign in
-              </Link>
-            </li>
-            <li className="ml-4">
-              <Link
-                href="/sign-up"
-                className="border p-2  text-sm border-slate-400  rounded-2xl bg-slate-700  hover:bg-slate-800 transition"
-              >
-                Sign up
-              </Link>
-            </li> */}
-          {/*  {!userId && (
-              <>
-                <li className="ml-8 ">
-                  <Link
-                    href="/sign-in"
-                    className="border border-slate-400 text-sm p-2  rounded-2xl bg-slate-700  hover:bg-slate-800 transition"
-                  >
-                    Sign in
-                  </Link>
-                </li>
-                <li className="ml-4">
-                  <Link
-                    href="/sign-up"
-                    className="border p-2  text-sm border-slate-400  rounded-2xl bg-slate-700  hover:bg-slate-800 transition"
-                  >
-                    Sign up
-                  </Link>
-                </li>
-              </>
-            )} */}
-          {/*       <li className="ml-6">
-              <UserButton afterSignOutUrl="/" />
-            </li> */}
-          {/*  <div> */}
         </ul>
       </div>
     </nav>
--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Navbar from "./components/navbar/navbar";
-import { ClerkProvider } from "@clerk/nextjs";
+
 import Footer from "./components/footer/footer";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -13,7 +13,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+ 
       <html lang="en">
         <body className={inter.className}>
           <Navbar />
@@ -21,6 +21,6 @@ export default function RootLayout({ children }) {
           <Footer />
         </body>
       </html>
-    </ClerkProvider>
+
   );
 }
